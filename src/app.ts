18826b60dcb6b24e634f2ee611297e1bf3db9a9f
vinicius <viniciusara2013@gmail.com>
2024-10-18 15:57:59 -0300

    :sparkles: Added books route

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,14 @@
 import express from "express";
 
 const app = express();
+const books = require("../data/books.json");
 
 app.get("/", (req, res) => {
 	res.status(200).send("Alura Curso NodeJs");
+});
+
+app.get("/books", (req, res) => {
+	res.status(200).json(books);
 });
 
 app.get("");
