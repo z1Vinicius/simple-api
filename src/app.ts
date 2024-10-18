7dcf68e93aef8450c7fd44ff264c7d851ad47b0a
Vinícius <viniciusara2013@gmail.com>
2024-10-18 20:58:56 -0300

    :construction: Added books tests

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,14 @@
 import express from "express";
 
 const app = express();
-const books = require("../data/books.json");
+app.use(express.json())
 
+interface IBook{
+	id: number
+	title: string
+}
+
+const books: IBook[] = require("../data/books.json");
 app.get("/", (req, res) => {
 	res.status(200).send("Alura Curso NodeJs");
 });
@@ -10,6 +16,14 @@ app.get("/", (req, res) => {
 app.get("/books", (req, res) => {
 	res.status(200).json(books);
 });
+
+app.post("/books", (req, res) => {
+	const id = Math.max.apply(Math, books.map(function(o) { return o.id; }))
+	const newBook: IBook = {id: id + 1, title: req.body.title}
+	books.push(newBook)
+	res.status(201).json(newBook)
+
+})
 
 app.get("");
 
