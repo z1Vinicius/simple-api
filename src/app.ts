b33638de98c9b04224308cf9425380d7e10be927
Vinícius <viniciusara2013@gmail.com>
2024-10-18 21:32:31 -0300

    :sparkles: Added patch and delete methods from books api

--- src/app.ts
+++ src/app.ts
@@ -22,7 +22,35 @@ app.post("/books", (req, res) => {
 	const newBook: IBook = {id: id + 1, title: req.body.title}
 	books.push(newBook)
 	res.status(201).json(newBook)
+})
 
+app.patch("/books/:id", (req, res) => {
+  try {
+    const bookData: IBook = req.body;
+		const bookId = req.params.id
+
+    const bookIndex = books.findIndex((book) => book.id === Number(bookId));
+    if (bookIndex === -1) {
+      res.status(404).json({ message: "Livro não encontrado" });
+			return
+    }
+    books[bookIndex] = { ...books[bookIndex], title: bookData.title };
+
+    res.status(200).json(books[bookIndex]);
+  } catch (error) {
+    res.status(500).json({ message: "Erro interno do servidor", error });
+  }
+});
+
+app.delete("/books/:id", (req, res)=> {
+	const bookId = req.params.id
+	const bookIndex = books.findIndex((book) => book.id === Number(bookId));
+	if (bookIndex === -1) {
+		res.status(404).json({ message: "Livro não encontrado" });
+		return
+	}
+	books.splice(bookIndex, 1)
+	res.status(200).send(`Livro ${bookId} deletado`)
 })
 
 app.get("");
